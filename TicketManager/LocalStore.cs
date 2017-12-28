77771b714846932a8b5c2175ebf9cd4b49278914
Neacsu Madalin <VonNeacsuMadalin@yahoo.com>
2017-12-28 11:07:26 +0200

    Added local store to store informations about current user.

---
+++ TicketManager/LocalStore.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace TicketManager
+{
+    class LocalStore
+    {
+        public static User currentUser = null;
+        public static ArrayList users = new ArrayList();
+    }
+}

99b5aa52948b409e17be099c46c709860dd26cb1
Neacsu Madalin <VonNeacsuMadalin@yahoo.com>
2017-12-28 11:06:23 +0200

    Addded dashboard view.

---
+++ TicketManager/Dashboard.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicketManager
+{
+    public partial class Dashboard : Form
+    {
+        public Dashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void maskedTextBox1_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+
+        private void Dashboard_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {            
+            FormStorer.Pop().Visible = false;
+            FormStorer.Peek().Visible = true;            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Visible = false;
+            FormStorer.Add("UserPanel", new UserPanel());
+            FormStorer.Get("UserPanel").Visible = true;
+        }
+    }
+}
---
+++ TicketManager/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicketManager
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            FormStorer.Add("Login", new Login());
+            Application.Run(FormStorer.Get("Login"));
+        }
+
+        
+    }
+}

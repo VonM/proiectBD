8a5e8186f181bbcf92b7d3f5758cbfa4eeaa2430
Neacsu Madalin <VonNeacsuMadalin@yahoo.com>
2017-12-28 11:07:45 +0200

    Added user role.

---
+++ TicketManager/Role.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketManager
+{
+    enum Role
+    {
+        ADMIN,        
+        TICKET_EDITOR,
+        EMPLOYEE,
+    };
+}

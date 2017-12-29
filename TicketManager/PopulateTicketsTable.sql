b8695cc7f54a1af8799864fb138acbecb919ed90
Neacsu Madalin <VonNeacsuMadalin@yahoo.com>
2017-12-29 21:15:56 +0200

    Populated tables even more.

--- TicketManager/PopulateTicketsTable.sql
+++ TicketManager/PopulateTicketsTable.sql
@@ -13,11 +13,15 @@ CREATE TABLE tickets (
 );
 
 INSERT INTO tickets (Name, FromUser, ToUser, Priority, Department, Category, Date, State, Description)
-	VALUES ('Fix UI bug', 'dorin', 'madalin', 1, 'Development', 'Bug', '20120618 10:34:09 AM', 'Started', 'When a player closes a window an exception is thrown. This must be fixed by handling the exception.');
+	VALUES ('Fix UI bug', 'madalin', 'raul', 1, 'Development', 'Bug', '20120618 10:34:09 AM', 'Started', 'When a player closes a window an exception is thrown. This must be fixed by handling the exception.');
 INSERT INTO tickets (Name, FromUser, ToUser, Priority, Department, Category, Date, State, Description)
-	VALUES ('Handle employees payment this month', 'marin', 'andrei', 0, 'Finance', 'Task', '20120618 10:34:09 AM', 'Review', 'We have to pay our employees this month.');
+	VALUES ('Handle employees payment this month', 'andrei', 'marin', 0, 'Finance', 'Task', '20120618 10:34:09 AM', 'Review', 'We have to pay our employees this month.');
 INSERT INTO tickets (Name, FromUser, ToUser, Priority, Department, Category, Date, State, Description)
-	VALUES ('Upgrade computers', 'madalin', 'cristi', 1, 'Marketing', 'Feature', '20120601 10:34:15 AM', 'Started', 'We need computers with better performance.');
+	VALUES ('Better market model', 'cristi', 'mihai', 1, 'Marketing', 'Feature', '20140601 10:34:15 AM', 'Started', 'We should consider using the Freemium model.');
 INSERT INTO tickets (Name, FromUser, ToUser, Priority, Department, Category, Date, State, Description)
-	VALUES ('Recruit new talent', 'cosmin', 'vasile', 3, 'Human_Resources', 'Story', '20130601 10:34:15 AM', 'Started', 'We could increase the number of our employees by searching and recruiting university students for summer internships.');
+	VALUES ('New Machine Learning algorithm', 'dorin', 'catalin', 1, 'Research_And_Development', 'Story', '20170604 10:40:15 AM', 'Started', 'A great performance improvement in Neural Networks has been discovered.');
+INSERT INTO tickets (Name, FromUser, ToUser, Priority, Department, Category, Date, State, Description)
+	VALUES ('Upgrade computers', 'mihaela', 'madalin', 1, 'Human_Resources', 'Feature', '20130701 10:34:15 AM', 'Started', 'We need computers with better performance.');
+INSERT INTO tickets (Name, FromUser, ToUser, Priority, Department, Category, Date, State, Description)
+	VALUES ('Recruit new talent', 'andreea', 'mihaela', 3, 'Human_Resources', 'Story', '20130601 10:34:15 AM', 'Started', 'We could increase the number of our employees by searching and recruiting university students for summer internships.');
 
--- TicketManager/PopulateUsersTable.sql
+++ TicketManager/PopulateUsersTable.sql
@@ -7,10 +7,14 @@ CREATE TABLE users (
 	Department varchar(255)
 );
 
-INSERT INTO users (Username, Password, Role, Department) VALUES ('madalin', '28E4DD049D9AADB6A0CBCC5798F3BE4A87EBD8754CBE1C0A8D89F27B27E9D64B', 'Admin', 'Development');
+INSERT INTO users (Username, Password, Role, Department) VALUES ('madalin', 'B24DC91D476F3429292E6CD76B52D8E2281446E7330BF206EA0376BB1CAC56BD', 'Admin', 'Development');
+INSERT INTO users (Username, Password, Role, Department) VALUES ('radu', 'E2561CB23FD849C14232C2897E231B00512F3B774A92E3134AE6E72ECDCF0AB7', 'Employee', 'Development');
+INSERT INTO users (Username, Password, Role, Department) VALUES ('raul', '42A79148E647CCA5D0BECAC0C0B5845CB0C11D80525F1464D59D7BE0FEEEB4FB', 'Ticket_Editor', 'Development');
 INSERT INTO users (Username, Password, Role, Department) VALUES ('andrei', 'B70C5216CA58902635529882E97C136069004E2E432C33CD6C7D0C96DD61E0D1', 'Employee', 'Finance');
-INSERT INTO users (Username, Password, Role, Department) VALUES ('marin', '00E932BB37C1F7468CE66F7430567A7DB91984EBA26B27811C34D3644C6C1FFD', 'Employee', 'Finance');
+INSERT INTO users (Username, Password, Role, Department) VALUES ('marin', '00E932BB37C1F7468CE66F7430567A7DB91984EBA26B27811C34D3644C6C1FFD', 'Ticket_Editor', 'Finance');
 INSERT INTO users (Username, Password, Role, Department) VALUES ('cristi', 'EF58F2A7554D79F9A00B5522F5C2D437FA931AF97B05843EE030967730F5AED0', 'Employee', 'Marketing');
-INSERT INTO users (Username, Password, Role, Department) VALUES ('dorin', '277B12C317720A0BA21BEB0FB7EB4C3BB44EBA7B32A3D98E97FC11FA20A4E2EA', 'Ticket_Editor', 'Research_And_Development');
-INSERT INTO users (Username, Password, Role, Department) VALUES ('vasile', '224FC9B678DE0EE4F719E0B06053E523C30F9C2EDE629322679C47264B58E72D', 'Ticket_Editor', 'Human_Resources');
-INSERT INTO users (Username, Password, Role, Department) VALUES ('cosmin', '66900B2138C834926F16A01E7EEEEE82865332B6D9089B8F7058BF952CC71631', 'Employee', 'Human_Resources');
+INSERT INTO users (Username, Password, Role, Department) VALUES ('mihai', 'EFACB25593CA596496451C9BB241E5071162812AB4DECD19EAA5A2E1F16F4385', 'Ticket_Editor', 'Marketing');
+INSERT INTO users (Username, Password, Role, Department) VALUES ('dorin', '277B12C317720A0BA21BEB0FB7EB4C3BB44EBA7B32A3D98E97FC11FA20A4E2EA', 'Employee', 'Research_And_Development');
+INSERT INTO users (Username, Password, Role, Department) VALUES ('catalin', '3A45209CD5E5FBC7C090A32FECF8CFA74C984E52CB074C2DB283EE6218EC387A', 'Ticket_Editor', 'Research_And_Development');
+INSERT INTO users (Username, Password, Role, Department) VALUES ('andreea', '968EDCAD0FA54C281B3CD1FDFE2B026BA2DC12271508B1E8581366CE20FBCCD7', 'Employee', 'Human_Resources');
+INSERT INTO users (Username, Password, Role, Department) VALUES ('mihaela', 'E393B684C25F5F364134F5CEAC428D42827CB4229F9A10A25F3B736FE28086A9', 'Ticket_Editor', 'Human_Resources');
